bf3267a406a98fbef08b25bfeb6317639a073cfd
Kuroda <guga.kenji1214@gmail.com>
2024-04-03 22:19:37 -0300

    config: Class 01

---
+++ Python/src/models/settings/base.py
@@ -0,0 +1,5 @@
+# Declare the database to SQLAlchemy
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
---
+++ Python/src/models/settings/connection.py
@@ -0,0 +1,31 @@
+# Connection handler for a sqlite database connection
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class __DBConnectionHandler:
+    def __init__(self) -> None:
+        self.__connection_string = "{}:///{}".format(
+            "sqlite",
+            "storage.db"
+        )
+        self.__engine = None
+        self.session = None
+
+    def connect_to_db(self) -> None:
+        self.__engine = create_engine(self.__connection_string)
+
+    def get_engine(self):
+        return self.__engine
+
+    def __enter__(self):
+        session_maker = sessionmaker()
+        self.session = session_maker(bind=self.__engine)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+
+# Uses only one instance of a database connection
+db_connection_handler = __DBConnectionHandler()
